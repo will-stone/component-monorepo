a95d53f2a4a7f4e691482faed69b30df28957991
Will Stone <654103+will-stone@users.noreply.github.com>
2022-01-09 21:12:18 +0000

    build from root script

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import babel from "@rollup/plugin-babel";
 import external from "rollup-plugin-peer-deps-external";
 import del from "rollup-plugin-delete";
 
-const pkg = require(process.env.INIT_CWD + "/package.json");
+const pkg = require(process.env.PWD + "/package.json");
 
 export default {
   input: pkg.source,
