4f4cf1f54cae821bc6ad9c4d6a7bd5764755f0f6
Will Stone <654103+will-stone@users.noreply.github.com>
2022-01-10 10:17:25 +0000

    use separate babel config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from "@rollup/plugin-babel";
 import external from "rollup-plugin-peer-deps-external";
 import del from "rollup-plugin-delete";
 
+const babelConfig = require("./babel.config.json");
 const pkg = require(process.env.PWD + "/package.json");
 
 export default {
@@ -17,23 +18,7 @@ export default {
       extensions: [".js", ".jsx", ".es6", ".es", ".mjs", ".ts", ".tsx"],
       babelHelpers: "runtime",
       exclude: "node_modules/**",
-      presets: [
-        "@babel/preset-typescript",
-        [
-          // Defaults to transform all code to ES5
-          // https://babeljs.io/docs/en/babel-preset-env
-          "@babel/preset-env",
-        ],
-        [
-          "@babel/preset-react",
-          {
-            // Use latest React JSX transform
-            // https://reactjs.org/blog/2020/09/22/introducing-the-new-jsx-transform.html
-            runtime: "automatic",
-          },
-        ],
-      ],
-      plugins: ["@babel/plugin-transform-runtime"],
+      ...babelConfig,
     }),
   ],
   external: [
